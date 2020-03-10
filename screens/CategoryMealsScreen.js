bdc3e4aa33a34b06e9d9408a406031060c07f3d0
Harmon Singh <harmon.singh@my.jcu.edu.au>
2020-03-10 17:59:42 +1000

    Initial Commit

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    return(
+        <View style={styles.screen}>
+            <Text>Category Meals Screen</Text>
+            <Button title="Meal Details" onPress={() => {
+                props.navigation.navigate({routeName: 'MealDetail'});
+            }} />
+        </View>
+    );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title
+    };
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default CategoryMealsScreen;
